944840643f8f05faf947b3b9e4b6931b0bc2db8d
deniskononenko <33topman@gmail.com>
2018-11-23 19:55:53 +0200

    first architecture

--- mygan.py
+++ mygan.py
@@ -11,16 +11,41 @@ import tensorflow
 
 class GAN():
     
-    def __init__():
+    def __init__(self):
         # dimensions of dimensions of input image for discriminator
         # image data (image_height, image_width, channels)
         self.image_height = 32
         self.image_width = 32
         self.channels = 1
-        
         self.generator = self.build_gen()
+        # build generator
         self.discriminator = self.build_discr()
+        # build discriminator
         
+    def build_gen():
+        """ Set up generator neural network. """
+        pass
+    
+    def build_discr():
+        """ Set up discriminator neural network. """
+        pass
+    
+    def train_generator():
+        """ 
+        Train Generator Neural Network. 
+        During generator training discriminator is static.
+        """
+        pass
+    
+    def train_discriminator():
+        """
+        Train Discriminator Neural Network 
+        During discriminator training generator is static.
+        """
+        pass
+    
+    
+
 
 
 if __name__ == "__main__":
