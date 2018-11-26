b6c831d363348ff5dbcd3a90ce739752f11156f1
deniskononenko <33topman@gmail.com>
2018-11-26 20:26:31 +0200

    frist working version of simple gan

---
+++ gan.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+def generator(z, hsize=[10, 10], reuse=False):
+    with tf.variable_scope("GAN\Generator", reuse=reuse):
+        print("test")
+
+    pass
+
+def discriminator():
+    pass
+
